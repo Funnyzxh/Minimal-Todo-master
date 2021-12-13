0da4e8dcf96da2d291c8482998df66a8774a627c
叶 柒 <1042238736@qq.com>
2021-12-13 18:31:23 +0800

    增加了周，月，年图表显示页面

---
+++ app/src/main/java/com/example/CAN301/timemanager/ChartShow/ShowWeekActivity.java
@@ -0,0 +1,55 @@
+package com.example.CAN301.timemanager.ChartShow;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.CAN301.timemanager.R;
+
+public class ShowWeekActivity extends AppCompatActivity{
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart_week);
+
+        final Drawable backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.weekbar);
+        setSupportActionBar(toolbar);
+        final Drawable backArrows = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        if (backArrows != null) {
+            backArrows.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        }
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        }
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(this) != null) {
+                    NavUtils.navigateUpFromSameTask(this);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
+
+
