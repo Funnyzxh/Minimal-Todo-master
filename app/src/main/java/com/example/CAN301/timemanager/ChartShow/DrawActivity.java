5cb6aec66be9282e570403938079918ee3385c8d
brucedoll <1104229153@qq.com>
2021-12-16 17:07:20 +0800

    修复画图bug

--- app/src/main/java/com/example/CAN301/timemanager/ChartShow/DrawActivity.java
+++ app/src/main/java/com/example/CAN301/timemanager/ChartShow/DrawActivity.java
@@ -54,16 +54,44 @@ public class DrawActivity extends View {
 
         public void drawPieChart() {
             //设置坐标
-            int left = x - 400;
-            int right = x + 400;
-            int top = y + 100 - 400;
-            int bottom = y + 100 + 400;
+            int left = x - x/2;
+            int right = x + x/2;
+            int top = y + y/12 - x/2;
+            int bottom = y + y/12 + x/2;
+            Log.d("test","runnedbf");
             ArrayList<ToDoItem> item= MainFragment.getLocallyStoredData(MainFragment.storeRetrieveData);
+            Log.d("test","runnedaf");
             float startAngle = 0;
             float sum=0;
-            float[] newsum=new float[10];
+            float[] newsum=new float[15];
             float newsum2=0;
             float currenttime=System.currentTimeMillis();
+            float percent=0;
+            Date now=new Date();
+            for(int i=0;i< item.size();i++) {
+                if (item.get(i).getmHasReminder() == false) {
+                    item.remove(i);
+                    i--;
+                }
+            }
+            long timenow=now.getTime()/600;
+            int newtimes=(int)timenow;
+            for(int i=0;i< item.size();i++) {
+                long endtimes=item.get(i).getToDoDate().getTime();
+                int endtime=(int)endtimes;
+                if(endtime-newtimes<=0){
+                    item.remove(i);
+                    i--;
+                }
+            }
+            for(int i=0;i< item.size();i++) {
+                if(item.get(i).getToDoDate().compareTo(now)<=0){
+                    item.remove(i);
+                    i--;
+                }
+            }
+
+
             for(int i=0;i< item.size();i++){
                 for(int j=0;j<item.size()-i-1;j++){
                     if(item.get(j).getToDoDate().getTime()<item.get(j+1).getToDoDate().getTime()){
@@ -85,31 +113,32 @@ public class DrawActivity extends View {
             for(int i=0;i<item.size();i++){
                 newsum2=newsum2+newsum[i];
             }
-            float percent=0;
+
             SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm");
-            for(int i=0;i<item.size();i++){
+            for(int i=item.size()-1;i>=0;i--){
                 if(item.get(i).getmHasReminder()){
-                    if(i<=item.size()/2) {
+                    if(item.size()==1) {
                         percent = 360 * (newsum[i] / newsum2);
-                        paint.setColor(Color.argb(255, 255*i/(item.size()/2), 255, 0));
+                        paint.setColor(Color.argb(200, 255, 0, 0));
                     }
-                    else{
+                    else {
                         percent = 360 * (newsum[i] / newsum2);
-                        paint.setColor(Color.argb(255, 255, 255-(i-item.size()/2)*255/(item.size()-item.size()/2), 0));
+                        paint.setColor(Color.argb(200, 255 * i / item.size(), 255 - 255 * i / item.size(), 0));
                     }
-                    mCancas.drawArc(left, top, right, bottom, startAngle, percent, true, paint);
-                    startAngle=startAngle+percent;
-                    mCancas.drawCircle(x/5,y/2+1000+(i+1)*200,50,paint);
-                    paint.setColor(Color.BLACK);
-                    paint.setTextSize(40);
-                    String newtime=sdf.format(item.get(i).getToDoDate());
-                    //int b=(int)(newsum[i] *100/ newsum2);
-                    mCancas.drawText(item.get(i).getToDoText()+" END AT "+newtime+" "+newsum[i] *100/ newsum2+"%",x/5+60,y/2+1010+(i+1)*200, paint);
+                        mCancas.drawArc(left, top, right, bottom, startAngle, percent, true, paint);
+                        startAngle=startAngle+percent;
+                        mCancas.drawCircle(x/5+x*3/40,y + y*7/48 + x/2+(item.size()-i)*x/4,40,paint);
+                        paint.setColor(Color.BLACK);
+                        paint.setTextSize(x/20);
+                        String newtime=sdf.format(item.get(i).getToDoDate());
+                        int b=(int)(newsum[i] *100/ newsum2);
+                        mCancas.drawText(item.get(i).getToDoText()+" END AT "+newtime+" "+b+"%",x/5+x*6/40,y + y*2/12 + x/2+(item.size()-i)*x/4, paint);
+
+
                 }
 
             }
             paint.setColor(Color.WHITE);
             mCancas.drawArc(left+100, top+100, right-100, bottom-100, 0, 360, true, paint);
-            //根据颜色和度数绘制饼状图
             }
     }
